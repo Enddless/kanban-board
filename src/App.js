96ca9e57167824510870a52216b843a01e288abf
Diana <iv_diana@bk.ru>
2023-03-30 19:59:08 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import response from './user-config'
+
+
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/header/header'
+import Main from './components/main/main'
+import Footer from './components/footer/footer'
 
 function App() {
+  const isLogged = response.activeUserId !== null
+
+  // в tasks по умолчанию будет храниться массив с данными из заглушки мок, в setTasks функция изменения состояния у задачек таскс
+  // чтобы эти данные были доступны в дочерних компонентах, их нужно передать в main
+
+  const initialState = JSON.parse(window.localStorage.getItem('tasks')) || []
+  const [tasks, setTasks] = useState(initialState); // вместо начальных значений из мок теперь считываем данные из localstorage
+
+  useEffect( () => {
+    window.localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header isLogged={isLogged}/>
+        <Main tasks={tasks} setTasks={setTasks} />
+        <Footer tasks={tasks}/>
+      </div>
+    </BrowserRouter>
   );
 }
 
